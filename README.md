4bac8afac5404cc501a00ef72b7a48cda85902fe
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-06-10 23:55:33 -0500

    Starting

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 RFC-CURP-Mexico
 ===============
 
-Getting RFC(Federal Taxpayer Register) and CURP(Unique Population Registry Code) for México
+Algorithm for getting RFC(Federal Taxpayer Register) and CURP(Unique Population Registry Code) for México, implemented in Javascript.
 
 Based on official Documents:
 
-CURP https://dl.dropbox.com/u/38547143/InstructivoParaLaCurp_v2008.pdf 
+CURP https://dl.dropbox.com/u/38547143/InstructivoParaLaCurp_v2008.pdf
+
 RFC https://dl.dropbox.com/u/38547143/GeneracionRFC.doc
---
+++ src/rfc.js
@@ -0,0 +1,25 @@
+
+var StringUtilities = {
+	getFirstVowel: function(word){
+		
+	}
+	clearString: function(word) {
+		string = word.trim();
+	}
+}
+
+var mxk = {
+	getCURP: function(name, surnameFather, surnameMother, bornDate, bornState, gender) {
+
+		rfc = this->getCommonPart(name, surnameFather, surnameFather,bornDate);
+		}
+
+	getCommonPart: function(name, surnameFather, surnameMother, bornDate) {
+		name 	= StringUtilities->clearString(name);
+		surnameFather = toUpperCase(surnameFather);
+		surnameMother = toUpperCase(surnameMother);
+
+		commonPart = 
+	}
+
+}
