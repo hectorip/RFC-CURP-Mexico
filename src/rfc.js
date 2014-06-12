d8ebd4eba8ebbe07f6ab11de3d5213de0d1a9334
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-06-11 23:52:52 -0500

    Adding common part of RFC and CURP

--- src/rfc.js
+++ src/rfc.js
@@ -1,25 +1,46 @@
 
 var StringUtilities = {
 	getFirstVowel: function(word){
-		
+		vowels = word.match(new RegExp('[AEIOU]'));
+		if(vowels) {
+			return vowels[0];
+		} else {
+			return 'X';
+		}
 	}
 	clearString: function(word) {
-		string = word.trim();
+		cleanWord = word.trim();
+		return cleanWord.toUpperCase();
 	}
 }
 
 var mxk = {
-	getCURP: function(name, surnameFather, surnameMother, bornDate, bornState, gender) {
 
-		rfc = this->getCommonPart(name, surnameFather, surnameFather,bornDate);
-		}
 
-	getCommonPart: function(name, surnameFather, surnameMother, bornDate) {
-		name 	= StringUtilities->clearString(name);
-		surnameFather = toUpperCase(surnameFather);
-		surnameMother = toUpperCase(surnameMother);
+	getCURP: 		function(name, surnameFather, surnameMother, bornDay, bornMonth, bornYear, bornState, gender) {
 
-		commonPart = 
-	}
+							rfc = this->getCommonPart(name, surnameFather, surnameFather, bornDay, bornMonth, bornYear);
+							
+					}
+
+	getCommonPart: 	function(name, surnameFather, surnameMother, bornDate) {
+		
+							name 	= StringUtilities->clearString(name);
+							surnameFather = StringUtilities->clearString(surnameFather);
+							surnameMother = StringUtilities->clearString(surnameMother);
+							bornDay =		StringUtilities->clearString(bornDay);
+							bornMonth =		StringUtilities->clearString(bornMonth);
+							bornYear =		StringUtilities->clearString(bornYear);
+
+							commonPart = surnameFather[0];
+							commonPart += StringUtilities->getFirstVowel(surnameFather);
+							commonPart += surnameMother[0];
+							commonPart += name[0];
+
+							commonPart += bornYear.substring(3,4);
+							commonPart += bornMonth;
+							commonPart += bornDay;
+						
+					}
 
 }
