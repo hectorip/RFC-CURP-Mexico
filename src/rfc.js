1fc1a401d6d942df9558e82dc936bf62d1d03fb2
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-06-21 00:27:03 -0500

    Common parta completed

--- src/rfc.js
+++ src/rfc.js
@@ -3,7 +3,7 @@ var StringUtilities = {
 	getFirstVowel: function(word){
 		vowels = word.match(new RegExp('[AEIOU]'));
 		return vowels[0] || 'X';
-	}
+	},
 	clearString: function(word) {
 		cleanWord = word.trim();
 		return cleanWord.toUpperCase();
@@ -15,28 +15,29 @@ var mxk = {
 
 	getCURP: 		function(name, surnameFather, surnameMother, bornDay, bornMonth, bornYear, bornState, gender) {
 
-							rfc = this->getCommonPart(name, surnameFather, surnameFather, bornDay, bornMonth, bornYear);
+							rfc = this.getCommonPart(name, surnameFather, surnameMother, bornDay, bornMonth, bornYear);
+							return rfc;
 
-					}
+					},
 
 	getCommonPart: 	function(name, surnameFather, surnameMother, bornDay, bornMonth, bornYear) {
 		
-							name 	= StringUtilities->clearString(name);
-							surnameFather = StringUtilities->clearString(surnameFather);
-							surnameMother = StringUtilities->clearString(surnameMother);
-							bornDay =		StringUtilities->clearString(bornDay);
-							bornMonth =		StringUtilities->clearString(bornMonth);
-							bornYear =		StringUtilities->clearString(bornYear);
+							name 	= StringUtilities.clearString(name);
+							surnameFather = StringUtilities.clearString(surnameFather);
+							surnameMother = StringUtilities.clearString(surnameMother);
+							bornDay =		StringUtilities.clearString(bornDay);
+							bornMonth =		StringUtilities.clearString(bornMonth);
+							bornYear =		StringUtilities.clearString(bornYear);
 
 							commonPart = surnameFather[0];
-							commonPart += StringUtilities->getFirstVowel(surnameFather);
+							commonPart += StringUtilities.getFirstVowel(surnameFather);
 							commonPart += surnameMother[0];
 							commonPart += name[0];
 
-							commonPart += bornYear.substring(3,4);
+							commonPart += bornYear.substring(2);
 							commonPart += bornMonth;
 							commonPart += bornDay;
-						
+							 return commonPart;
 					}
 
 }
