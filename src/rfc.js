06ebd3dd2448e3aef022631d5e19dfeaea2aa7fe
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-02-28 12:13:22 -0600

    Adding fill state select

--- src/rfc.js
+++ src/rfc.js
@@ -248,88 +248,97 @@ var mxk = {
         "VUEY": "VXEY",
         "WUEI": "WXEI",
         "WUEY": "WXEY"
-    }
+    },
 
     badWordsRFC : [
-                    "BUEI": "BUEX",
-                    "BUEY": "BUEX",
-                    "CACA": "CACX",
-                    "CACO": "CACX",
-                    "CAGA": "CAGX",
-                    "CAGO": "CAGX",
-                    "CAKA": "CAKX",
-                    "COGE": "COGX",
-                    "COJA": "COJX",
-                    "COJE": "COJX",
-                    "COJI": "COJX",
-                    "COJO": "COJX",
-                    "CULO": "CULX",
-                    "FETO": "FETX",
-                    "GUEY": "GUEX",
-                    "JOTO": "JOTX",
-                    "KACA": "KACX",
-                    "KACO": "KACX",
-                    "KAGA": "KAGX",
-                    "KAGO": "KAGX",
-                    "KOGE": "KOGX",
-                    "KOJO": "KOJX",
-                    "KAKA": "KAKX",
-                    "KULO": "KULX",
-                    "MAME": "MAMX",
-                    "MAMO": "MAMX",
-                    "MEAR": "MEAX",
-                    "MEON": "MEOX",
-                    "MION": "MIOX",
-                    "MOCO": "MOCX",
-                    "MULA": "MULX",
-                    "PEDA": "PEDX",
-                    "PEDO": "PEDX",
-                    "PENE": "PENX",
-                    "PUTA": "PUTX",
-                    "PUTO": "PUTX",
-                    "QULO": "QULX",
-                    "RATA": "RATX",
-                    "RUIN": "RUIX",
+                    "BUEI",
+                    "BUEY",
+                    "CACA",
+                    "CACO",
+                    "CAGA",
+                    "CAGO",
+                    "CAKA",
+                    "COGE",
+                    "COJA",
+                    "COJE",
+                    "COJI",
+                    "COJO",
+                    "CULO",
+                    "FETO",
+                    "GUEY",
+                    "JOTO",
+                    "KACA",
+                    "KACO",
+                    "KAGA",
+                    "KAGO",
+                    "KOGE",
+                    "KOJO",
+                    "KAKA",
+                    "KULO",
+                    "MAME",
+                    "MAMO",
+                    "MEAR",
+                    "MEON",
+                    "MION",
+                    "MOCO",
+                    "MULA",
+                    "PEDA",
+                    "PEDO",
+                    "PENE",
+                    "PUTA",
+                    "PUTO",
+                    "QULO",
+                    "RATA",
+                    "RUIN",
                 ],
-    characterValues
-                "0" :  '00'
-                "1" :  '01' 
-                "2" :  '02'
-                "3" :  '03'
-                "4" :  '04'
-                "5" :  '05'
-                "6" :  '06'
-                "7" :  '07'
-                "8" :  '08'
-                "9" :  '09'
-                "A" :  '10'
-                "B" :  '11'
-                "C" :  '12'
-                "D" :  '13'
-                "F" :  '15'
-                "E" :  '14'
-                "G" :  '16'
-                "H" :  '17'
-                "I" :  '18'
-                "J" :  '19'
-                "K" :  '20'
-                "L" :  '21'
-                "M" :  '22'
-                "N" :  '23'
-                "&" :  '24'
-                "O" :  '25'
-                "P" :  '26'
-                "Q" :  '27'
-                "R" :  '28'
-                "S" :  '29' 
-                "T" :  '30' 
-                "U" :  '31' 
-                "V" :  '32' 
-                "W" :  '33' 
-                "X" :  '34' 
-                "Y" :  '35' 
-                "Z" :  '36' 
-                " " :  '37' 
+    characterValues: {
+                "0" :  '00',
+                "1" :  '01',
+                "2" :  '02',
+                "3" :  '03',
+                "4" :  '04',
+                "5" :  '05',
+                "6" :  '06',
+                "7" :  '07',
+                "8" :  '08',
+                "9" :  '09',
+                "A" :  '10',
+                "B" :  '11',
+                "C" :  '12',
+                "D" :  '13',
+                "F" :  '15',
+                "E" :  '14',
+                "G" :  '16',
+                "H" :  '17',
+                "I" :  '18',
+                "J" :  '19',
+                "K" :  '20',
+                "L" :  '21',
+                "M" :  '22',
+                "N" :  '23',
+                "&" :  '24',
+                "O" :  '25',
+                "P" :  '26',
+                "Q" :  '27',
+                "R" :  '28',
+                "S" :  '29',
+                "T" :  '30',
+                "U" :  '31',
+                "V" :  '32',
+                "W" :  '33',
+                "X" :  '34',
+                "Y" :  '35',
+                "Z" :  '36',
+                " " :  '37',
                 "Ñ" :  '38' 
-                ]
+                },
+    fillBornStateSelect: function(field_id){
+        element = document.getElementById(field_id);
+        console.log(element)
+        this.states.forEach(function(state){
+            option = new Option(state.name, state.value)
+            element.appendChild(option)
+        });
+        
+    }
 }
